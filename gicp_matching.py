7be121323344ab3e8f8c03a060ec7cbc64aa345b
ngtroku <131967935+ngtroku@users.noreply.github.com>
2024-05-04 14:34:51 +0900

    Add files via upload

--- gicp_matching.py
+++ gicp_matching.py
@@ -12,8 +12,8 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    file_name1 = "./data/000101.bin"
-    csv_file_name2 = "./data/14_rouka_person_at128_2.csv"
+    file_name1 = "./data/000118.bin"
+    csv_file_name2 = "./data/14_000101.bin"
     sampling_rate = 0.5 #点群全体から抽出する点数の割合を決定
     scale_rotation = 3 #回転移動の分散
     scale_translation = 1 #並行移動の分散
@@ -23,10 +23,10 @@ if __name__ == "__main__":
 
     print("rotate x:{} rotate y:{} rotate z:{} noise x:{} noise y:{}".format(rotation_x[0], rotation_y[0], rotation_z[0], noise_x[0], noise_y[0]))
 
-    #source_raw_numpy, target_raw_numpy = factor.preprocess_manual(csv_file_name2, sampling_rate, rotation_x[0], rotation_y[0], rotation_z[0], noise_x[0], noise_y[0])
+    #source_raw_numpy, target_raw_numpy = factor.preprocess_manual(csv_file_name2, sampling_rate, rotation_x[0], rotation_y[0], rotation_z[0], noise_x[0], noise_y[0], file_format = "bin", lidar_model="VLP-16")
     #points_source2, importance_factor2 = factor.calc_factor(source_raw_numpy, target_raw_numpy)
 
-    factor.postprocess([points_source1], [importance_factor1], ["./result_101.csv"])
+    factor.postprocess([points_source1], [importance_factor1], ["./result_118.csv"])
 
     #print(importance_factor1, importance_factor2)
 
--- visualizer.py
+++ visualizer.py
@@ -3,7 +3,7 @@ import open3d as o3d
 import pandas as pd
 import numpy as np
 import matplotlib 
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import itertools
 
 def csv_2_array(file_name):
@@ -26,10 +26,24 @@ def cmap(value):
     return colors
     """
 
-xyz, eigenvalue = csv_2_array("./result_campus.csv")
-colors = cmap(eigenvalue)
+def show_hist(eigen_value):
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
 
-pointcloud1 = o3d.geometry.PointCloud()
-pointcloud1.points = o3d.utility.Vector3dVector(xyz)
-pointcloud1.colors = o3d.utility.Vector3dVector(colors)
-o3d.visualization.draw_geometries([pointcloud1])
+    ax.hist(eigen_value, bins=40)
+    ax.set_xlim(0.0, 1.0)
+    ax.set_xticks([0.1 * i for i in range(11)])
+    ax.set_xlabel("eigen value score")
+    plt.show()
+
+if __name__ == "__main__":
+    file_name = input("Please set visualize file path:")
+    xyz, eigenvalue = csv_2_array(str(file_name))
+    colors = cmap(eigenvalue)
+
+    pointcloud1 = o3d.geometry.PointCloud()
+    pointcloud1.points = o3d.utility.Vector3dVector(xyz)
+    pointcloud1.colors = o3d.utility.Vector3dVector(colors)
+    o3d.visualization.draw_geometries([pointcloud1])
+
+    show_hist(eigenvalue)
